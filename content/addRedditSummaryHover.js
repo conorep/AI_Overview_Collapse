ff2b11934a8b9195fbae7df5c4692a8d60a92586
conorep <91850829+conorep@users.noreply.github.com>
2026-05-29 14:12:38 -0700

    adding scroll listener * long comment sections can have truncated comments so scroll listener handles this best * removed previously-added return logic * removed setTimeOut function call * implementing classList handling to ensure multiple style rule duplicates don't get added to shreddit-comment elements

--- content/addRedditSummaryHover.js
+++ content/addRedditSummaryHover.js
@@ -1,18 +1,16 @@
-let hasRun = false;
 const addHoverBackground = () => {
-	if(hasRun) return;
-
 	const shredditComments = document.querySelectorAll('shreddit-comment');
 	const newSheetStyle = new CSSStyleSheet();
 	newSheetStyle.insertRule('summary div.flex.relative:hover { background-color: rgba(255, 255, 255, 0.1); }')
 
 	shredditComments.forEach((shComment) => {
-		hasRun = true;
 		const commentShadowRoot = shComment.shadowRoot;
-		if(commentShadowRoot)
-			commentShadowRoot.adoptedStyleSheets = [...commentShadowRoot.adoptedStyleSheets, newSheetStyle]
+		if(!shComment.classList.contains('alreadyAdded') && commentShadowRoot) {
+			shComment.classList.add('alreadyAdded');
+			commentShadowRoot.adoptedStyleSheets = [...commentShadowRoot.adoptedStyleSheets, newSheetStyle];
+		}
 	})
 }
 
 document.onreadystatechange = addHoverBackground;
-setTimeout(addHoverBackground, 3000);
+document.addEventListener('scroll', addHoverBackground);
