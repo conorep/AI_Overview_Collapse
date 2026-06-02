c7d20640d77567f11fffd86642f4e92fc65a972f
conorep <91850829+conorep@users.noreply.github.com>
2026-06-02 15:53:45 -0700

    fixing reddit post overflow-x issue * latest update to reddit webpage created problem with an element's overflow-x display    * was showing empty scroll area * finding element in shadowRoot and unsetting overflow-x scrollable area

--- content/removeRedditAds.js
+++ content/removeRedditAds.js
@@ -4,19 +4,33 @@ const adRemoval = (elementSelector) => {
     targetElements.forEach((el) => el.remove());
 }
 
-const dynamicAdRemoval = () => adRemoval('div[slot="credit-bar"]:not(div#pdp-credit-bar), div[slot="ad-format-content"],  shreddit-dynamic-ad-link');
+const fixActionRowOverflow = () => {
+  let postContent = document.querySelector('shreddit-post');
+  if(!postContent) return;
+
+  let shadowDoc = postContent.shadowRoot;
+  if(!shadowDoc) return;
+
+  let targetElement = shadowDoc.querySelector('div.shreddit-post-container');
+  if(targetElement)
+    targetElement.style.overflowX = 'auto';
+}
+
+const dynamicAdRemoval = () => {
+  adRemoval('div[slot="credit-bar"]:not(div#pdp-credit-bar), div[slot="ad-format-content"],  shreddit-dynamic-ad-link');
+  fixActionRowOverflow();
+};
 
 const readyStateAdRemoval = () => {
   if(document.readyState !== 'complete')
     return;
   dynamicAdRemoval();
+  fixActionRowOverflow();
 }
 
 const shredditAdRemoval = () => adRemoval('shreddit-ad-post');
 
 window.onload = dynamicAdRemoval;
-
 document.addEventListener('readystatechange', readyStateAdRemoval);
-
 document.addEventListener('scroll', shredditAdRemoval);
 shredditAdRemoval();
